a4c272276786de0c730f85b97379d8c2ac2ec17a
Oleg Parashchenko <olpa@uucode.com>
2022-01-19 06:02:26 +0100

    Get files from `multi-nlg`, pass tests.

---
+++ tests/util/fixture.py
@@ -0,0 +1,42 @@
+import json
+import os
+import sys
+from json import JSONDecodeError
+from typing import Mapping
+
+
+def load_string_file(file_name: str) -> Mapping[str, str]:
+    path_to_fixture = os.path.join(os.path.dirname(__file__), '..', 'fixture')
+    f = os.path.join(path_to_fixture, file_name)
+    mapping = {}
+    with open(f) as h:
+        for line in h:
+            if '=' not in line:
+                continue
+            (id_, s) = line.split('=', 2)
+            s = s.strip()
+            if s.startswith('$'):
+                s = mapping[s[1:]]
+            mapping[id_.strip()] = s
+    return mapping
+
+
+def load_file(file_name: str) -> Mapping[str, object]:
+    parses = {}
+    for id_, s in load_string_file(file_name).items():
+        try:
+            parse = json.loads(s)
+            parses[id_] = parse
+        except JSONDecodeError:
+            print(
+                'fixture.load_file: can not load as json: >>%s<<' % s,
+                file=sys.stderr)
+    return parses
+
+
+def load_camxes_parses() -> Mapping[str, object]:
+    return load_file('camxes_parse.txt')
+
+
+def load_lcs() -> Mapping[str, object]:
+    return load_file('lcs.txt')
